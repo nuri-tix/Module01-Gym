30fb2baf331a2365e5c622e14485cf4b552025de
nuri-tix <artweb1509@gmail.com>
2022-01-06 23:52:16 +0300

    Add Files

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
+// Модальное окно
+
 function openModal() {
-   document.getElementById("popupWindow").style.left = "35%";
+   document.getElementById("popupWindow").style.left = "28%";
    document.querySelector(".blackout").style.display = 'block';
 }
 
 function closeModal() {
    document.getElementById("popupWindow").style.left = "100%";
    document.querySelector(".blackout").style.display = 'none';
+}
+
+// Button Up
+
+window.onscroll = function () {
+   scrollFunction()
+};
+
+const upbuttons = document.querySelectorAll(".button-up");
+
+for (const upbutton of upbuttons) {
+   upbutton.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+   e.preventDefault();
+   const href = this.getAttribute("href");
+
+   document.querySelector(href).scrollIntoView({
+      behavior: "smooth"
+   });
+}
+
+function scrollFunction() {
+   if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+      document.getElementById('btnUp').className = 'button-up visible';
+   } else {
+      document.getElementById('btnUp').className = 'button-up hidden';
+   }
 }
