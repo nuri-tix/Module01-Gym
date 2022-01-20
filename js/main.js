00bdf3546e41414036dd76db0bc0921c72381ec4
nuri-tix <artweb1509@gmail.com>
2022-01-20 15:11:11 +0300

    Add Files

--- js/main.js
+++ js/main.js
@@ -11,32 +11,32 @@ function closeModal() {
 
 // Button Up
 
-window.onscroll = function () {
-   scrollFunction()
-};
+// window.onscroll = function () {
+//    scrollFunction()
+// };
 
-const upbuttons = document.querySelectorAll(".button-up");
+// const upbuttons = document.querySelectorAll(".button-up");
 
-for (const upbutton of upbuttons) {
-   upbutton.addEventListener("click", clickHandler);
-}
+// for (const upbutton of upbuttons) {
+//    upbutton.addEventListener("click", clickHandler);
+// }
 
-function clickHandler(e) {
-   e.preventDefault();
-   const href = this.getAttribute("href");
+// function clickHandler(e) {
+//    e.preventDefault();
+//    const href = this.getAttribute("href");
 
-   document.querySelector(href).scrollIntoView({
-      behavior: "smooth"
-   });
-}
+//    document.querySelector(href).scrollIntoView({
+//       behavior: "smooth"
+//    });
+// }
 
-function scrollFunction() {
-   if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-      document.getElementById('btnUp').className = 'button-up visible';
-   } else {
-      document.getElementById('btnUp').className = 'button-up hidden';
-   }
-}
+// function scrollFunction() {
+//    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+//       document.getElementById('btnUp').className = 'button-up visible';
+//    } else {
+//       document.getElementById('btnUp').className = 'button-up hidden';
+//    }
+// }
 
 
 
